bd1c338eb9f158ffb4cf9436ebdfda7585995f55
IrisSaskia <irisoostra@upcmail.nl>
2021-11-25 11:05:42 +0100

    Bob en kleding colliden vanaf nu

---
+++ Assets/Scripts/AvatarDressing.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class AvatarDressing : MonoBehaviour
+{
+    [SerializeField]
+    private string clothingTagName = "Clothing";
+
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnCollisionEnter(Collision collision) {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if(collision.gameObject.name == "MyGameObjectName") {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            Debug.Log("Do something here");
+        }
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if(collision.gameObject.tag == clothingTagName) {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Do something else here");
+        }
+    }
+}
