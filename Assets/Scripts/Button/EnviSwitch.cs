ad9ef4e340752f197d7eb6e76cd78d5d7b0e52f7
ikarian7 <scott.marchant7@gmail.com>
2021-12-09 11:48:25 +0100

    button made

--- Assets/Scripts/Button/EnviSwitch.cs
+++ Assets/Scripts/Button/EnviSwitch.cs
@@ -2,38 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrimaryReactor : MonoBehaviour
+public class EnviSwitch : MonoBehaviour
 {
-    public PrimaryButtonWatcher watcher;
-    public bool IsPressed = false; // used to display button state in the Unity Inspector window
     public Vector3 descentSpeed = new Vector3(1, 5, 1);
     public float descentDuration = 0.25f; // seconds
     private Vector3 offPosition;
-    private Vector3 onPosition;
+    public Vector3 onPosition = new Vector3(1, 3, 1);
     private Coroutine descent;
     [SerializeField]
     private GameObject bushes;
 
+    private bool used = false;
+
     void Start()
     {
-        watcher.primaryButtonPress.AddListener(onPrimaryButtonEvent);
         offPosition = bushes.transform.position;
-        onPosition = Vector3.Scale(descentSpeed, offPosition);
+        //onPosition = Vector3.Scale(descentSpeed, offPosition);
     }
 
-    public void onPrimaryButtonEvent(bool pressed)
+    public void Switch()
     {
-        IsPressed = pressed;
         if (descent != null)
+        {
             StopCoroutine(descent);
-        if (pressed)
+        }
+        if (!used)
+        {
             bushes.SetActive(true);
             descent = StartCoroutine(AnimateDescent(bushes.transform.position, onPosition));
-        //else
-        //    descent = StartCoroutine(AnimateDescent(this.transform.position, offPosition));
+            used = true;
+        }
     }
 
-    public IEnumerator AnimateDescent(Vector3 fromPosition, Vector3 toPosition)
+    private IEnumerator AnimateDescent(Vector3 fromPosition, Vector3 toPosition)
     {
         float t = 0;
         while (t < descentDuration)
