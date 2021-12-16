4d5d956064957a74d2f553661f0d13acfad243f2
IrisSaskia <irisoostra@upcmail.nl>
2021-12-16 13:59:11 +0100

    Switch between body parts done

--- Assets/Scripts/Character Creator/Blendshapes/TransformBody.cs
+++ Assets/Scripts/Character Creator/Blendshapes/TransformBody.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TransformBody : MonoBehaviour {
     bool isBusy = false;
+
+    [SerializeField]
+    private Text selectedBodyPart;
 
     [SerializeField]
     private float timeForScaling = 1f;
@@ -22,6 +26,12 @@ public class TransformBody : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+
+        foreach(Blendshape selectedShape in blendshapes) {
+            if(selectedShape.isSelected) {
+                selectedBodyPart.text = selectedShape.shapeName;
+            }
+        }
     }
 
     public void ScaleStart(bool isThisButtonRight) {
@@ -79,5 +89,39 @@ public class TransformBody : MonoBehaviour {
         isBusy = false;
 
         yield return null;
+    }
+
+    public void SwitchBlendshape(bool isThisButtonUp) {
+        int start = 0;
+        int end = blendshapes.Count-1;
+        for(int b = 0; b <= end; b++) {
+            if(blendshapes[b].isSelected) {
+                if(!isThisButtonUp) {
+                    if(b != end) {
+                        blendshapes[(b + 1)].isSelected = true;
+                        Debug.Log("state 1");
+                    } else {
+                        blendshapes[start].isSelected = true;
+                        Debug.Log("state 2");
+                    }
+                } else {
+                    if(b != start) {
+                        blendshapes[(b - 1)].isSelected = true;
+                        Debug.Log("state 3");
+                    } else {
+                        blendshapes[end].isSelected = true;
+                        Debug.Log("state 4");
+                    }
+                }
+                Debug.Log("b is " + b);
+                blendshapes[b].isSelected = false;
+                break;
+            }
+        }
+        foreach(Blendshape selectedShape in blendshapes) {
+            if(selectedShape.isSelected) {
+                selectedBodyPart.text = selectedShape.shapeName;
+            }
+        }
     }
 }
